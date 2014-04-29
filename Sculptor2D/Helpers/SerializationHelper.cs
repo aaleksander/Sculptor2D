49e2d731fa916eb06b4819ae128dd7ad836bc8a1
aaleksander <aleksandranufriev@yandex.ru>
2014-04-29 09:56:07 +0400

    1

---
+++ Sculptor2D/Helpers/SerializationHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+
+namespace DrawToolsLib
+{
+}
